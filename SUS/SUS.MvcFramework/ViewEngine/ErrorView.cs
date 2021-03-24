f1599a83af3830eb60698d60668544a96af8b15b
Georgi Vasilev <goshko09@gmail.com>
2021-03-24 16:45:35 +0200

    Introduce view engine

---
+++ SUS/SUS.MvcFramework/ViewEngine/ErrorView.cs
@@ -0,0 +1,33 @@
+﻿namespace SUS.MvcFramework.ViewEngine
+{
+    using System.Linq;
+    using System.Text;
+    using System.Collections.Generic;
+
+    public class ErrorView : IView
+    {
+        private IEnumerable<string> errors;
+        private string csharpCode;
+
+        public ErrorView(IEnumerable<string> errors, string csharpCode)
+        {
+            this.errors = errors;
+            this.csharpCode = csharpCode;
+        }
+
+        public string ExecuteTemplate(object viewModel)
+        {
+            var html = new StringBuilder();
+            html.AppendLine($"<h1> View compile {this.errors.Count()} errors:</h1><ul>");
+
+            foreach (var error in this.errors)
+            {
+                html.AppendLine($"<li>{error}</li>");
+            }
+
+            html.AppendLine($"</ul><pre>{csharpCode}</pre>");
+
+            return html.ToString();
+        }
+    }
+}
